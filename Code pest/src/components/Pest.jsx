bea602117eec56e15ed23ddba2db77d81c49e339
AshaminBiswas <152035553+AshaminBiswas@users.noreply.github.com>
2025-05-02 20:49:54 +0530

    done

--- Code pest/src/components/Pest.jsx
+++ Code pest/src/components/Pest.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 
 function Pest() {
-  return (
-    <div>
+  const pests = useSelector((state) => state.pest.pest);
 
+  console.log(pests);
+
+  const dispatch = useDispatch()
+  const [searchTitle, setSearchTitle] = useState("")
+  const filteredData = pests.filter((pest) => pest.title.toLowerCase().includes(searchTitle.toLowerCase()))
+
+
+  return (
+    <div className='px-8 py-4'>
+      <h1 className='text-center text-3xl font-semibold '>List of Pests</h1>
+
+      <div className='flex justify-center p-4 items-center'>
+        <input type="search"
+          placeholder='Search Your Pest'
+          value={searchTitle}
+          onChange={(e) => setSearchTitle(e.target.value)}
+          className='w-1/2 p-4 rounded-3xl outline-none border'
+        />
+      </div>
+      <div>
+        {
+          filteredData.length > 0 &&
+          filteredData.map((pest) => {
+            return (
+              <div>
+
+                <div>
+                  {pest.title}
+                </div>
+                <div>
+                  {pest.value}
+                </div>
+                <div className='flex gap-4'>
+                  <button>edit</button>
+                  <button>copy</button>
+                  <button>delete</button>
+                  <button>view</button>
+                  <button>share</button>
+                </div>
+
+              </div>
+            )
+          })
+
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Pest
+export default Pest;
+
